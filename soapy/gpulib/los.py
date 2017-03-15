35674493befad83e08fa41157fc7b0a82a2a9515
Andrew Reeves <a.p.reeves@durham.ac.uk>
2017-03-15 13:13:44 +0000

    Fix out of bounds bug

--- soapy/gpulib/los.py
+++ soapy/gpulib/los.py
@@ -44,19 +44,19 @@ def bilinear_interp_kernel(data, xCoords, yCoords, interpArray):
     """
     # Thread id in a 1D block
     i, j = cuda.grid(2)
+    if i < interpArray.shape[0] and j < interpArray.shape[1]:
+        # Get corresponding coordinates in image
+        x = xCoords[i]
+        x1 = numba.int32(x)
+        y = yCoords[j]
+        y1 = numba.int32(y)
 
-    # Get corresponding coordinates in image
-    x = xCoords[i]
-    x1 = numba.int32(x)
-    y = yCoords[j]
-    y1 = numba.int32(y)
+        # Do bilinear interpolation
+        xGrad1 = data[x1+1, y1] - data[x1, y1]
+        a1 = data[x1, y1] + xGrad1*(x-x1)
 
-    # Do bilinear interpolation
-    xGrad1 = data[x1+1, y1] - data[x1, y1]
-    a1 = data[x1, y1] + xGrad1*(x-x1)
+        xGrad2 = data[x1+1, y1+1] - data[x1, y1+1]
+        a2 = data[x1, y1+1] + xGrad2*(x-x1)
 
-    xGrad2 = data[x1+1, y1+1] - data[x1, y1+1]
-    a2 = data[x1, y1+1] + xGrad2*(x-x1)
-
-    yGrad = a2 - a1
-    interpArray[i,j] = a1 + yGrad*(y-y1)
+        yGrad = a2 - a1
+        interpArray[i,j] = a1 + yGrad*(y-y1)
